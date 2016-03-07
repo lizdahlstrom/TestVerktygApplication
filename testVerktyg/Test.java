4e5c81863813391295cd6aa1bc6d4e9460956b54
Joakim <rtuu41@gmail.com>
2016-03-07 13:08:50 +0100

    Creating test database class

--- testVerktyg/Test.java
+++ testVerktyg/Test.java
@@ -1,8 +1,20 @@
 package testVerktyg;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
+@Table
 public class Test {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int tId;
+	@ManyToOne
+	private int qId;
 
 }
