19e407be845edc30ddb9e8b63b34552e5a5d51ef
Joakim <rtuu41@gmail.com>
2016-03-08 13:59:48 +0100

    Inlogmodel if block

--- testVerktyg/InlogModel.java
+++ testVerktyg/InlogModel.java
@@ -4,28 +4,29 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-
 public class InlogModel {
-	
-	private User user = new User();
-	private Boolean bool =false;
 
-	public boolean isAdmin(String name, String pw){ 
+	private User user = new User();
+	private Boolean bool = false;
+
+	public boolean isAdmin(String name, String pw) {
 		EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("Eclipselink_JPA");
 		EntityManager entitymanager = emfactory.createEntityManager();
-		if((user = entitymanager.find(User.class, name)) != null){
-			bool=true;
-			
-			
+		if ((user = entitymanager.find(User.class, name)) != null
+				&& (user = entitymanager.find(User.class, name)) != null) {
+
+			if ((user = entitymanager.find(User.class, user)).isAdmin()) {
+
+			}
+
+			// bool=true;
+
 		}
-		
-		
-		
-		
+
 		return bool;
-		
+
 	}
-	
+
 }
 
-//if sats jämföra string name och string pw om det är admin returna true
+// if sats jämföra string name och string pw om det är admin returna true
