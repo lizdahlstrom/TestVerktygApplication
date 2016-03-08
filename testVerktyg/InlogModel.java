3ef8b0f678adc5dc0fce60ec473d35e3a8aec548
Soniiqaah <Soniiqaah@Sonicas-MacBook-Pro.local>
2016-03-08 11:56:08 +0100

    håller på och fixar inlogModel databas

--- testVerktyg/InlogModel.java
+++ testVerktyg/InlogModel.java
@@ -1,9 +1,20 @@
 package testVerktyg;
 
 public class InlogModel {
+	
+	private String name;
+	private String pw;
 
 	public boolean isAdmin(String name, String pw){ 
-		// connect to db here
+		
+		
+		
+		
+		
 		return true;
+		
 	}
+	
 }
+
+//if sats jämföra string name och string pw om det är admin returna true
