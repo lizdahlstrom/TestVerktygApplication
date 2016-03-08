3837798bad8ce59aa758d9bedcad0498afb6c500
Soniiqaah <Soniiqaah@Sonicas-MacBook-Pro.local>
2016-03-08 11:57:23 +0100

    håller på och fixar inlog Model databas

--- testVerktyg/InlogModel.java
+++ testVerktyg/InlogModel.java
@@ -1,17 +1,28 @@
 package testVerktyg;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+
 public class InlogModel {
 	
-	private String name;
-	private String pw;
+	private User user = new User();
+	private Boolean bool =false;
 
 	public boolean isAdmin(String name, String pw){ 
+		EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("Eclipselink_JPA");
+		EntityManager entitymanager = emfactory.createEntityManager();
+		if((user = entitymanager.find(User.class, name)) != null){
+			bool=true;
+			
+			
+		}
 		
 		
 		
 		
-		
-		return true;
+		return bool;
 		
 	}
 	
