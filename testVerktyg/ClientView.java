6c2f125fdab61e63e379dbc88c03efab26b8e9e6
Soniiqaah <Soniiqaah@Sonicas-MacBook-Pro.local>
2016-03-09 14:27:06 +0100

    Writing comment in ClientView

--- testVerktyg/ClientView.java
+++ testVerktyg/ClientView.java
@@ -24,11 +24,11 @@ public class ClientView {
 	private VBox centerLayout;
 	private HBox topHbox;
 
-
-	private Button btnNext = new Button("Nästa");
+	// Creating buttons
+	private Button btnNext = new Button("Nï¿½sta");
 	private Button btnPrev = new Button("Tillbaka");
-	private Button btnSend = new Button("Lämna in");
-
+	private Button btnSend = new Button("Lï¿½mna in");
+	// Creating labels
 	private Label lblTitle;
 	private Label lblDescript;
 	private Label lblTimer;
@@ -44,7 +44,7 @@ public class ClientView {
 		//Labels
 		lblTitle = new Label("Starta test");
 		lblTitle.setFont(Font.font(32));
-		lblDescript = new Label("Starta testet genom att trycka på Nästa..");
+		lblDescript = new Label("Starta testet genom att trycka pï¿½ Nï¿½sta..");
 		lblDescript.setFont(Font.font(16));
 		lblTimer = new Label("00:00");
 		lblTimer.setFont(Font.font(16));
