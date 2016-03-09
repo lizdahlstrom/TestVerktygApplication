6d52220a0d1d7a9b88e6284e844b1ddab82f2954
Joakim <rtuu41@gmail.com>
2016-03-09 11:37:37 +0100

    inlogmodel moving emfactory

--- testVerktyg/InlogModel.java
+++ testVerktyg/InlogModel.java
@@ -2,7 +2,6 @@ package testVerktyg;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
 public class InlogModel {
 
@@ -10,9 +9,10 @@ public class InlogModel {
 	private User pass = new User();
 	private Boolean bool = false;
 
-	public boolean isAdmin(String name, String pw) {
-		EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("Eclipselink_JPA");
-		EntityManager entitymanager = emfactory.createEntityManager();
+	public boolean isAdmin(String name, String pw, EntityManagerFactory emfactory, EntityManager entitymanager) {
+		// EntityManagerFactory emfactory =
+		// Persistence.createEntityManagerFactory("Eclipselink_JPA");
+		// EntityManager entitymanager = emfactory.createEntityManager();
 		if ((user = entitymanager.find(User.class, name)) != null && (pass = entitymanager.find(User.class, pw)) != null
 				&& user.getId() == pass.getId()) {
 
@@ -26,9 +26,10 @@ public class InlogModel {
 
 	}
 
-	public boolean isPupil(String name, String pw) {
-		EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("Eclipselink_JPA");
-		EntityManager entitymanager = emfactory.createEntityManager();
+	public boolean isPupil(String name, String pw, EntityManagerFactory emfactory, EntityManager entitymanager) {
+		// EntityManagerFactory emfactory =
+		// Persistence.createEntityManagerFactory("Eclipselink_JPA");
+		// EntityManager entitymanager = emfactory.createEntityManager();
 		if ((user = entitymanager.find(User.class, name)) != null && (pass = entitymanager.find(User.class, pw)) != null
 				&& user.getId() == pass.getId()) {
 			if ((user = entitymanager.find(User.class, user)).isAdmin() == false) {
