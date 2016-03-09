1ef4033fdea47436422336a829024b2d0de16589
Soniiqaah <Soniiqaah@Sonicas-MacBook-Pro.local>
2016-03-09 13:51:40 +0100

    fixing comments

--- testVerktyg/InlogModel.java
+++ testVerktyg/InlogModel.java
@@ -2,20 +2,21 @@ package testVerktyg;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+// This class is the Model for InlogView which contains two if statements.
 
 public class InlogModel {
-
+// Instance variables
 	private Users user = new Users();
 	private Users pass = new Users();
 	private Boolean bool = false;
 
+//Method isAdmin there the method get the parameters mentioned below.
 	public boolean isAdmin(String name, String pw, EntityManagerFactory emfactory, EntityManager entitymanager) {
-		// EntityManagerFactory emfactory =
-		// Persistence.createEntityManagerFactory("Eclipselink_JPA");
-		// EntityManager entitymanager = emfactory.createEntityManager();
+
+//Checking if name and password == true or != null	
 		if ((user = entitymanager.find(Users.class, name)) != null && (pass = entitymanager.find(Users.class, pw)) != null
 				&& user.getId() == pass.getId()) {
-
+//Checking if user == isAdmin
 			if ((user = entitymanager.find(Users.class, user)).isAdmin()) {
 				bool = true;
 			}
@@ -25,11 +26,10 @@ public class InlogModel {
 		return bool;
 
 	}
-
+//Method isPupil there the method get the parameters mentioned below.
 	public boolean isPupil(String name, String pw, EntityManagerFactory emfactory, EntityManager entitymanager) {
-		// EntityManagerFactory emfactory =
-		// Persistence.createEntityManagerFactory("Eclipselink_JPA");
-		// EntityManager entitymanager = emfactory.createEntityManager();
+
+//Checking if user == isPupil if it is isAdmin == false. 
 		if ((user = entitymanager.find(Users.class, name)) != null && (pass = entitymanager.find(Users.class, pw)) != null
 				&& user.getId() == pass.getId()) {
 			if ((user = entitymanager.find(Users.class, user)).isAdmin() == false) {
@@ -43,4 +43,4 @@ public class InlogModel {
 
 }
 
-// if sats jämföra string name och string pw om det är admin returna true
+
--- testVerktyg/TestEntity.java
+++ testVerktyg/TestEntity.java
@@ -6,6 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+//Denna klassen talar 
 @Entity
 @Table
 public class TestEntity {
