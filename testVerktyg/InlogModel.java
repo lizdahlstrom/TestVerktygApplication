189dbc884910e1adbbe965e9c4fc3d326f4a515a
Liz Dahlström <liz.dahlstrom@gmail.com>
2016-03-11 20:51:39 +0100

    Inlog validation and exception handling done

--- testVerktyg/InlogModel.java
+++ testVerktyg/InlogModel.java
@@ -2,6 +2,7 @@ package testVerktyg;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.NoResultException;
 import javax.persistence.Query;
 // This class is the Model for InlogView which contains two if statements.
 
@@ -13,28 +14,29 @@ public class InlogModel {
 	private int uId;
 	private Query query;
 	private int test;
+	private String errorMsg = "Uknown error...";
 
 	// Method isAdmin there the method get the parameters mentioned below.
-	public Boolean isAdmin(String name, String pw, EntityManagerFactory emfactory, EntityManager em) {
+	public Boolean isAdmin(String name, String pw, EntityManager em) {
 
 		query = em.createNamedQuery("User.findUserByNameAndPw", User.class);
 
 		query.setParameter("uName", name);
 		query.setParameter("uPass", pw);
-		try{
+		try {
 			// Checking if name and password == true or != null
 			user = (User) query.getSingleResult();
-			//		if (!user.equals(null)) {
+			// if (!user.equals(null)) {
 
 			if (user.getIsAdmin() == true) {
 				bool = true;
 
 			}
-		}catch(Exception e){
-			System.out.println("Login exception: " + e);
+		} catch (Exception e) {
+			handleNoResultException(e);
+			return false;
 		}
 		return bool;
-
 	}
 
 	public int getUId() {
@@ -46,24 +48,35 @@ public class InlogModel {
 		query = em.createNamedQuery("User.findUserByNameAndPw", User.class);
 		query.setParameter("uName", name);
 		query.setParameter("uPass", pw);
-		try{
+		try {
 			if (!user.equals(null)) {
 				user = (User) query.getSingleResult();
 				if (user.getIsAdmin() == false) {
 					bool = true;
 				}
 
-			}}catch(Exception e){
-				System.out.println("Login exception: " + e);
 			}
+		} catch (NoResultException e) {
+			handleNoResultException(e);
+			return false;
+		}
 
 		return bool;
 	}
 
-	public boolean isInputValid(String str){
-		if(str != null && str.length()<30){
+	public boolean isInputValid(String str) {
+		if (str != null && !str.isEmpty() && str.length() < 30) {
 			return true;
-		}		
+		}
 		return false;
+	}
+
+	private void handleNoResultException(Exception e) {
+		System.out.println("Login exception: " + e);
+		errorMsg = new String("Login exception: Incorrect username or password.");
+	}
+
+	public String getErrorMsg() {
+		return errorMsg;
 	}
 }
--- testVerktyg/TestToolApp.java
+++ testVerktyg/TestToolApp.java
@@ -5,16 +5,19 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
- * 
+ *
  * JAVAFX Application
- * 
+ *
  */
 
 public class TestToolApp extends Application {
-	//Instance variables
+	// Instance variables
 	private InlogView inlogView;
 	private InlogModel inlogModel;
 	private ClientController clientContr;
@@ -32,54 +35,49 @@ public class TestToolApp extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		this.primaryStage = primaryStage;
-		inlogModel = new InlogModel();
-		inlogView = new InlogView();
+
 		emfactory = Persistence.createEntityManagerFactory("Eclipselink_JPA");
 		em = emfactory.createEntityManager();
 
-		loginStart();
-
+		// Temporary VBox with button ... needed to launch login window
+		Button button = new Button("Login");
+		VBox vboxs = new VBox();
+		vboxs.getChildren().add(button);
+		button.setOnAction(e -> {
+			loginStart();
+		});
+		primaryStage.setScene(new Scene(vboxs));
+		primaryStage.show();
 	}
 
-	private void loginStart(){
-		if(inlogModel.isInputValid(inlogView.getName()) && inlogModel.isInputValid(inlogView.getPass())){
+	public void loginStart() {
+		inlogModel = new InlogModel();
+		inlogView = new InlogView();
+		if (inlogModel.isInputValid(inlogView.getName()) && inlogModel.isInputValid(inlogView.getPass())) {
 
-			Boolean isAdmin = inlogModel.isAdmin(inlogView.getName(), inlogView.getPass(), emfactory, em);
+			Boolean isAdmin = inlogModel.isAdmin(inlogView.getName(), inlogView.getPass(), em);
 			userId = inlogModel.getUId();
 
-			if(isAdmin){
+			if (isAdmin) {
 				adminContr = new AdminController(primaryStage, em, userId);
 				primaryStage.setTitle(title + ": Admin View");
 				primaryStage.setScene(adminContr.getView().getAdminScene());
-				primaryStage.show();
-			}
-			else if(!isAdmin && inlogModel.isPupil(inlogView.getName(), inlogView.getPass(), emfactory, em)){
-				clientContr = new ClientController(primaryStage,em, userId);
+			} else if (!isAdmin && inlogModel.isPupil(inlogView.getName(), inlogView.getPass(), emfactory, em)) {
+				clientContr = new ClientController(primaryStage, em, userId);
 				primaryStage.setTitle(title + ": Client View");
 				primaryStage.setScene(clientContr.getView().getTestView());
-				primaryStage.show();
+			} else {
+				System.out.println(inlogModel.getErrorMsg());
 			}
-			else{
-				System.out.println("Login failed...");
-				resetLogin();
-			}
-		}
-		else{
+		} else {
 			System.out.println("Login failed... invalid format for input.");
-			resetLogin();
-		}		
-	}
-
-	private void resetLogin(){
-		inlogView = new InlogView();
-		inlogModel = new InlogModel();
-		loginStart();
+		}
+		primaryStage.show();
 	}
 
 	@Override
-	public void stop() throws Exception{
+	public void stop() throws Exception {
 		em.close();
 		emfactory.close();
 	}
-
 }
