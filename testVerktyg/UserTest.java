d567470127f0167c166240491f0d14c241756ad4
Liz Dahlström <liz.dahlstrom@gmail.com>
2016-03-16 20:32:14 +0100

    Added named queries

--- testVerktyg/UserTest.java
+++ testVerktyg/UserTest.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
@@ -15,19 +16,24 @@ import javax.persistence.Table;
  */
 @Entity
 @Table(name = "user_test")
-@NamedQuery(name = "UserTest.findAll", query = "SELECT u FROM UserTest u")
+@NamedQueries({ @NamedQuery(name = "UserTest.findAll", query = "SELECT u FROM UserTest u"),
+		@NamedQuery(name = "UserTest.findById", query = "SELECT u FROM UserTest u WHERE u.id = :id"),
+		@NamedQuery(name = "UserTest.findByUserId", query = "SELECT u FROM UserTest u WHERE u.user.uId = :userId"),
+		@NamedQuery(name = "UserTest.findByTestId", query = "SELECT u FROM UserTest u WHERE u.test.testId = :testId"),
+		@NamedQuery(name = "UserTest.findUsersByTestId", query = "SELECT u.user FROM UserTest AS u WHERE u.test.testId =:testId"),
+		@NamedQuery(name = "UserTest.findTestsByUserId", query = "SELECT u.test FROM UserTest AS u WHERE u.user.uId =:userId") })
 public class UserTest implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	private int id;
 
-	// bi-directional many-to-one association to Test2
+	// bi-directional many-to-one association to Test
 	@ManyToOne
 	@JoinColumn(name = "testId")
 	private Test test;
 
-	// bi-directional many-to-one association to User2
+	// bi-directional many-to-one association to User
 	@ManyToOne
 	@JoinColumn(name = "userId")
 	private User user;
